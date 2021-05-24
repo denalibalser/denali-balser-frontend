33c7c2ef3cd150461be9aa794edc918bf32488cd
denalibalser <denali.balser167@gmail.com>
2021-05-24 10:22:52 -0500

    add redux dir and files

---
+++ denali-bbalser-frontend/src/redux/index.js
@@ -0,0 +1,18 @@
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
+// import authReducer from './reducers/authReducer'
+// import speciesReducer from './reducers/speciesReducer' 
+import thunk from 'redux-thunk'
+
+const rootReducer = combineReducers({
+    // auth: authReducer, 
+    // species: speciesReducer 
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk))
+);
+
+export default store;
